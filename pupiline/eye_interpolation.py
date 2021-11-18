4cf59c8a7dd727ec6bcebaf8a782c293b2c4937f
7cm-diameter <from.this.ridiculous.loop@gmail.com>
2021-11-18 13:13:14 +0900

    Apply formatter

--- pupiline/eye_interpolation.py
+++ pupiline/eye_interpolation.py
@@ -1,10 +1,10 @@
+import glob
 from pathlib import Path
 from typing import List, Tuple
+
 import numpy as np
 import pandas as pd
 from nptyping import NDArray
-import glob
-
 from pandas.core.frame import DataFrame
 
 Bodyparts = str
@@ -19,7 +19,8 @@ def remove_scorer(data: pd.DataFrame):
     return data.set_axis(idx, axis=1)
 
 
-def replace_low_likelihood_nan(data: pd.DataFrame, threshold: float) -> NDArray :
+def replace_low_likelihood_nan(data: pd.DataFrame,
+                               threshold: float) -> NDArray:
     likelihood_over_threshold = data > threshold
     return np.array(likelihood_over_threshold[likelihood_over_threshold])
 
@@ -36,15 +37,16 @@ def contains_likelihood(key: DLCKey):
     return "likelihood" in key
 
 
-def make_dataframe_before_interpolated (outlitter_to_nan : NDArray,idx_list : list) -> pd.DataFrame:
+def make_dataframe_before_interpolated(outlitter_to_nan: NDArray,
+                                       idx_list: list) -> pd.DataFrame:
 
-    Dataframe_before_interpolated = pd.DataFrame(data = outlitter_to_nan)
+    Dataframe_before_interpolated = pd.DataFrame(data=outlitter_to_nan)
     idx = pd.MultiIndex.from_tuples(idx_list)
-    Dataframe_before_interpolated.set_axis(idx, axis=1,inplace=True)
+    Dataframe_before_interpolated.set_axis(idx, axis=1, inplace=True)
     return Dataframe_before_interpolated
 
 
-def as_output_filename(h5_path: Path,data_name: str):
+def as_output_filename(h5_path: Path, data_name: str):
     parent = h5_path.parents[0]
     stem = h5_path.stem.split("DLC")[0]
     filepath_without_extension = parent.joinpath("interpolated_data").joinpath(
@@ -52,49 +54,48 @@ def as_output_filename(h5_path: Path,data_name: str):
     return str(filepath_without_extension) + data_name + ".h5"
 
 
-
 if __name__ == '__main__':
 
-
     h5p = "where/the/DLC/h5/files/is"
-    h5s = glob.glob(h5p+"*.h5")
+    h5s = glob.glob(h5p + "*.h5")
     likelihood_threshold = 0.9
 
     for h5 in h5s:
 
         print(f"processing " + h5)
-        
+
         tracked_data = pd.read_hdf(h5)
         tracked_data = remove_scorer(tracked_data)
 
-        x_idx = list(filter(contains_x,tracked_data.keys()))
-        y_idx = list(filter(contains_y,tracked_data.keys()))
-        likelihood_data = list(filter(contains_likelihood,tracked_data.keys()))
+        x_idx = list(filter(contains_x, tracked_data.keys()))
+        y_idx = list(filter(contains_y, tracked_data.keys()))
+        likelihood_data = list(filter(contains_likelihood,
+                                      tracked_data.keys()))
 
         x_array = np.array(tracked_data[x_idx])
         y_array = np.array(tracked_data[y_idx])
-        likelihood_array_replaced = replace_low_likelihood_nan(tracked_data[likelihood_data],likelihood_threshold)
+        likelihood_array_replaced = replace_low_likelihood_nan(
+            tracked_data[likelihood_data], likelihood_threshold)
 
-        x_outlitter_to_nan =  x_array * likelihood_array_replaced
-        y_outlitter_to_nan =  y_array * likelihood_array_replaced
-        
-        
-        x_Dataframe_before_interpolated = make_dataframe_before_interpolated(x_outlitter_to_nan,x_idx)
-        y_Dataframe_before_interpolated = make_dataframe_before_interpolated(y_outlitter_to_nan,y_idx)
+        x_outlitter_to_nan = x_array * likelihood_array_replaced
+        y_outlitter_to_nan = y_array * likelihood_array_replaced
 
-        data_before_interpolated = pd.concat([x_Dataframe_before_interpolated,y_Dataframe_before_interpolated ],axis=1)
-        data_before_interpolated.sort_index(axis = 1,inplace=True)
+        x_Dataframe_before_interpolated = make_dataframe_before_interpolated(
+            x_outlitter_to_nan, x_idx)
+        y_Dataframe_before_interpolated = make_dataframe_before_interpolated(
+            y_outlitter_to_nan, y_idx)
 
-        data_after_interpolated = data_before_interpolated.astype("float64").interpolate()
+        data_before_interpolated = pd.concat(
+            [x_Dataframe_before_interpolated, y_Dataframe_before_interpolated],
+            axis=1)
+        data_before_interpolated.sort_index(axis=1, inplace=True)
 
-        output_path_nan = as_output_filename(Path(h5),"_outlitter_to_NaN")
-        output_path_interpolated = as_output_filename(Path(h5),"_interpolated")
-        
-        data_before_interpolated.to_hdf(output_path_nan,"key")
-        data_after_interpolated.to_hdf(output_path_interpolated,"key")
+        data_after_interpolated = data_before_interpolated.astype(
+            "float64").interpolate()
 
+        output_path_nan = as_output_filename(Path(h5), "_outlitter_to_NaN")
+        output_path_interpolated = as_output_filename(Path(h5),
+                                                      "_interpolated")
 
-
- 
- 
- 
+        data_before_interpolated.to_hdf(output_path_nan, "key")
+        data_after_interpolated.to_hdf(output_path_interpolated, "key")

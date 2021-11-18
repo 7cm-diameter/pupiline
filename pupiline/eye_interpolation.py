80543a8a1584ad3289b8f8718fd018130bd6b26a
7cm-diameter <from.this.ridiculous.loop@gmail.com>
2021-11-18 13:15:42 +0900

    Remove unused import

--- pupiline/eye_interpolation.py
+++ pupiline/eye_interpolation.py
@@ -5,7 +5,6 @@ from typing import List, Tuple
 import numpy as np
 import pandas as pd
 from numpy.typing import NDArray
-from pandas.core.frame import DataFrame
 
 Bodyparts = str
 Coordinate = str
